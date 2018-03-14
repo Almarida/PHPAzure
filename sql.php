50816b58e6251848045ff40ea385a875751e551a
WeekendUpdate <corey.haggarty@gmail.com>
2018-03-14 16:10:28 -0700

    Update sql.php

--- sql.php
+++ sql.php
@@ -1,7 +1,8 @@
 <?php
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:testdbca.database.windows.net,1433; Database = testdbc", "chaggarty", "!@Welcome123");
+//    $conn = new PDO("sqlsrv:server = tcp:testdbca.database.windows.net,1433; Database = testdbc", "chaggarty", "!@Welcome123");
+    $conn = new PDO("sqlsrv:server = tcp:10.1.0.4,1433; Database = test", "chaggarty", "!@Welcome123");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
