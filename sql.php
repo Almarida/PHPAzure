1ed4028064a497c7c3161abfbf7624fb450a40ba
WeekendUpdate <corey.haggarty@gmail.com>
2018-03-14 11:37:30 -0700

    Update sql.php

--- sql.php
+++ sql.php
@@ -1,17 +1,14 @@
-<?php
 // PHP Data Objects(PDO) Sample Code:
-
 try {
     $conn = new PDO("sqlsrv:server = tcp:testdbca.database.windows.net,1433; Database = testdbc", "chaggarty", "!@Welcome123");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
-if( $conn ) {
-     echo "Connection established.<br />";
-}else{
-     echo "Connection could not be established.<br />";
-     die( print_r( sqlsrv_errors(), true));
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
 }
+
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "chaggarty@testdbca", "pwd" => "{your_password_here}", "Database" => "testdbc", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "chaggarty@testdbca", "pwd" => "!@Welcome123", "Database" => "testdbc", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:testdbca.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
