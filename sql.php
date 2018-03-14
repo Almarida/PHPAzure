91fbbdeb520932e5c8a2ba81500b140c3e1d87e4
WeekendUpdate <corey.haggarty@gmail.com>
2018-03-14 11:24:53 -0700

    Update sql.php

--- sql.php
+++ sql.php
@@ -1,15 +1,17 @@
 <?php
-$serverName = "serverName\sqlexpress"; //serverName\instanceName
-
-// Since UID and PWD are not specified in the $connectionInfo array,
-// The connection will be attempted using Windows Authentication.
-$connectionInfo = array( "Database"=>"dbName");
-$conn = sqlsrv_connect( $serverName, $connectionInfo);
-
-if( $conn ) {
-     echo "Connection established.<br />";
-}else{
-     echo "Connection could not be established.<br />";
-     die( print_r( sqlsrv_errors(), true));
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:testdbca.database.windows.net,1433; Database = testdbc", "chaggarty", "{your_password_here}");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "chaggarty@testdbca", "pwd" => "{your_password_here}", "Database" => "testdbc", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:testdbca.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
 ?>
